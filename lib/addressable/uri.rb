6ad5ab7405e42bcbe62d140f4b5d3783fd2cfc0c
Bob Aman <bob@sporkmonger.com>
2008-06-05 15:03:47 -0400

    Updated copyright.

--- lib/addressable/uri.rb
+++ lib/addressable/uri.rb
@@ -1,5 +1,5 @@
 #--
-# Addressable, Copyright (c) 2006-2007 Bob Aman
+# Addressable, Copyright (c) 2006-2008 Bob Aman
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
