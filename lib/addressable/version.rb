d341c39f7b99a14b2b41ffb79beb0170467120d1
Bob Aman <bob@sporkmonger.com>
2009-01-30 22:18:49 -0500

    Updated version number and CHANGELOG.

--- lib/addressable/version.rb
+++ lib/addressable/version.rb
@@ -27,7 +27,7 @@ if !defined?(Addressable::VERSION)
     module VERSION #:nodoc:
       MAJOR = 2
       MINOR = 0
-      TINY  = 1
+      TINY  = 2
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
