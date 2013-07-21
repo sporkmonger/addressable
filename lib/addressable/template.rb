5b9e6ea6c525d83f944814196899bb62d0d959b2
Andrey Popp <8mayday@gmail.com>
2013-07-22 01:36:43 +0400

    Addressable::Template#match accepts optional block

--- lib/addressable/template.rb
+++ lib/addressable/template.rb
@@ -390,7 +390,12 @@ module Addressable
       unparsed_values = uri.to_str.scan(expansion_regexp).flatten
 
       if uri.to_str == pattern
-        return Addressable::Template::MatchData.new(uri, self, mapping)
+        m = Addressable::Template::MatchData.new(uri, self, mapping)
+        if block_given?
+          yield m
+        else
+          return m
+        end
       elsif expansions.size > 0
         index = 0
         expansions.each do |expansion|
@@ -443,7 +448,12 @@ module Addressable
             index = index + 1
           end
         end
-        return Addressable::Template::MatchData.new(uri, self, mapping)
+        m = Addressable::Template::MatchData.new(uri, self, mapping)
+        if block_given?
+          yield m
+        else
+          return m
+        end
       else
         return nil
       end
