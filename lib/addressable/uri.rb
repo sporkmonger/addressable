69a6ba7ad3899b9c2215adb1d963748e568035a9
Tim Craft <mail@timcraft.com>
2013-11-02 20:22:40 +0000

    Fix character class bug/warnings

--- lib/addressable/uri.rb
+++ lib/addressable/uri.rb
@@ -505,7 +505,7 @@ module Addressable
       end
       if character_class.kind_of?(String)
         leave_re = if leave_encoded.length > 0
-          character_class << '%'
+          character_class = "#{character_class}%" unless character_class.include?('%')
 
           "|%(?!#{leave_encoded.chars.map do |char|
             seq = char.unpack('C*').map { |c| '%02x' % c }.join
