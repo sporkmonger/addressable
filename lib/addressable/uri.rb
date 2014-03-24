e4ae2b84acbda91db7fca760074bdeeba9b782bb
Bob Aman <bob@sporkmonger.com>
2014-03-23 22:21:48 -0700

    Make sure the output of normalization is UTF-8.

--- lib/addressable/uri.rb
+++ lib/addressable/uri.rb
@@ -536,6 +536,9 @@ module Addressable
       rescue ArgumentError
         encoded = self.encode_component(unencoded)
       end
+      if encoded.respond_to?(:force_encoding)
+        encoded.force_encoding(Encoding::UTF_8)
+      end
       return encoded
     end
 
