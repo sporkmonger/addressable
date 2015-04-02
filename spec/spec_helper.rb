6e4e22cceb92a6586cd2806c6f206d8f0216a793
Bob Aman <bob@sporkmonger.com>
2015-04-02 14:00:36 +0300

    Ignore anything that's been vendored.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,14 @@ begin
   require 'coveralls'
   Coveralls.wear! do
     add_filter "spec/"
+    add_filter "vendor/"
   end
 rescue LoadError
   warn "warning: coveralls gem not found; skipping Coveralls"
   require 'simplecov'
   SimpleCov.start do
     add_filter "spec/"
+    add_filter "vendor/"
   end
 end
 
