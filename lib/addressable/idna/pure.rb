68c6fb80eae8bd4ee137b0ccabff144bdfc41732
Akira Matsuda <ronnie@dio.jp>
2017-09-01 19:53:46 +0900

    :warning: assigned but unused variable - startercc

--- lib/addressable/idna/pure.rb
+++ lib/addressable/idna/pure.rb
@@ -151,11 +151,9 @@ module Addressable
         if (starter_cc == 0 &&
             (composite = unicode_compose_pair(starter, ch)) != nil)
           starter = composite
-          startercc = lookup_unicode_combining_class(composite)
         else
           unpacked_result << starter
           starter = ch
-          startercc = cc
         end
       end
       unpacked_result << starter
