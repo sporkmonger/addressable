992ef0c5144a900774ecf2f2cc05aa0d8d6b2e18
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2019-01-19 00:58:10 +0500

    refactor validations into dedicated methods

--- lib/addressable/validator.rb
+++ lib/addressable/validator.rb
@@ -4,12 +4,12 @@ module Addressable
   ##
   # Ensures that the URI is valid.
   module Validator
-    METHODS_POOL = %w[
+    VALIDATE_METHODS = %w[
       scheme user password userinfo host authority
       origin port path fragment
     ].freeze
 
-    METHODS_POOL.each do |m|
+    VALIDATE_METHODS.each do |m|
       define_method "#{m}=".to_sym do |*args|
         super(*args)
         validate
@@ -21,42 +21,65 @@ module Addressable
     def validate
       return if @validation_deferred
 
-      if !scheme.nil? && ip_based? &&
-         (host.nil? || host.empty?) &&
-         (path.nil? || path.empty?)
-        raise URI::InvalidURIError,
-              "Absolute URI missing hierarchical segment: '#{self}'"
+      if missing_hierarchical_segment?
+        error_msg = "Absolute URI missing hierarchical segment: '#{self}'"
+        raise URI::InvalidURIError, error_msg
       end
 
-      if host.nil?
-        if !port.nil? || !user.nil? || !password.nil?
-          raise URI::InvalidURIError, "Hostname not supplied: '#{self}'"
+      if hostname_not_supplied?
+        raise URI::InvalidURIError, "Hostname not supplied: '#{self}'"
+      end
+
+      if path_presence
+        if relative_path? && !authority.nil?
+          error_msg = 'Cannot have a relative path'
+          error_msg += " with an authority set: '#{self}'"
+          raise URI::InvalidURIError, error_msg
+        end
+
+        if path_with_two_leading_slashes? && authority.nil?
+          error_msg = 'Cannot have a path with two leading slashes'
+          error_msg += " without an authority set: '#{self}'"
+          raise URI::InvalidURIError, error_msg
         end
       end
 
-      if !path.nil? && !path.empty?
-        if path[0..0] != URI::SLASH && !authority.nil?
-          raise URI::InvalidURIError,
-                "Cannot have a relative path with an authority set: '#{self}'"
-        end
-
-        if path[0..1] == URI::SLASH + URI::SLASH && authority.nil?
-          raise URI::InvalidURIError,
-                "Cannot have a path with two leading slashes without an authority set: '#{self}'"
-        end
-      end
-
-      unreserved = URI::CharacterClasses::UNRESERVED
-      sub_delims = URI::CharacterClasses::SUB_DELIMS
-
-      if !host.nil? && (host =~ %r/[<>{}\/\\\?\#\@"[[:space:]]]/ ||
-          (!host[/^\[(.*)\]$/, 1].nil? && host[/^\[(.*)\]$/, 1] !~
-              Regexp.new("^[#{unreserved}#{sub_delims}:]*$")))
-
+      if !host.nil? && invalid_host_character?
         raise URI::InvalidURIError, "Invalid character in host: '#{host}'"
       end
 
       nil
+    end
+
+    def missing_hierarchical_segment?
+      !scheme.nil? && ip_based? && host.to_s.empty? && path.to_s.empty?
+    end
+
+    def hostname_not_supplied?
+      host.nil? && (!port.nil? || !user.nil? || !password.nil?)
+    end
+
+    def path_presence
+      !path.nil? && !path.empty?
+    end
+
+    def relative_path?
+      path[0..0] != URI::SLASH
+    end
+
+    def path_with_two_leading_slashes?
+      path[0..1] == URI::SLASH + URI::SLASH
+    end
+
+    def invalid_host_character?
+      (host =~ %r/[<>{}\/\\\?\#\@"[[:space:]]]/ || unescaped_backslashes?)
+    end
+
+    def unescaped_backslashes?
+      unreserved = URI::CharacterClasses::UNRESERVED
+      sub_delims = URI::CharacterClasses::SUB_DELIMS
+      regexp     = Regexp.new("^[#{unreserved}#{sub_delims}:]*$")
+      !host[/^\[(.*)\]$/, 1].nil? && host[/^\[(.*)\]$/, 1] !~ regexp
     end
   end
 end
