c1ba2fd056a9ccc7044ead46f0a3aa8e9442c92d
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2020-11-15 19:05:25 +0500

    split checks in validate method into dedicated methods

--- lib/addressable/validator.rb
+++ lib/addressable/validator.rb
@@ -9,34 +9,55 @@ module Addressable
     def validate
       return if @validation_deferred
 
-      if missing_hierarchical_segment?
-        error_msg = "Absolute URI missing hierarchical segment: '#{self}'"
-        raise URI::InvalidURIError, error_msg
-      end
-
-      if hostname_not_supplied?
-        raise URI::InvalidURIError, "Hostname not supplied: '#{self}'"
-      end
-
-      if path_presence?
-        if relative_path? && !authority.nil?
-          error_msg = "Cannot have a relative path"
-          error_msg += " with an authority set: '#{self}'"
-          raise URI::InvalidURIError, error_msg
-        end
-
-        if path_with_two_leading_slashes? && authority.nil?
-          error_msg = "Cannot have a path with two leading slashes"
-          error_msg += " without an authority set: '#{self}'"
-          raise URI::InvalidURIError, error_msg
-        end
-      end
-
-      if !host.nil? && invalid_host_character?
-        raise URI::InvalidURIError, "Invalid character in host: '#{host}'"
-      end
+      check_hierarchical_segment
+      check_hostname_supplied
+      check_path_presence
+      check_invalid_host_characters
 
       nil
+    end
+
+    def check_hierarchical_segment
+      return unless missing_hierarchical_segment?
+
+      error_msg = "Absolute URI missing hierarchical segment: '#{self}'"
+      raise URI::InvalidURIError, error_msg
+    end
+
+    def check_path_presence
+      return unless path_presence?
+
+      check_relative_path
+      check_path_with_two_leading_slashes
+    end
+
+    def check_relative_path
+      return unless relative_path?
+
+      error_msg = "Cannot have a relative path"
+      error_msg += " with an authority set: '#{self}'"
+      raise URI::InvalidURIError, error_msg
+    end
+
+    def check_path_with_two_leading_slashes
+      return unless path_with_two_leading_slashes?
+
+      error_msg = "Cannot have a path with two leading slashes"
+      error_msg += " without an authority set: '#{self}'"
+      raise URI::InvalidURIError, error_msg
+    end
+
+    def check_invalid_host_characters
+      return if host.nil?
+      return unless invalid_host_character?
+
+      raise URI::InvalidURIError, "Invalid character in host: '#{host}'"
+    end
+
+    def check_hostname_supplied
+      return unless hostname_not_supplied?
+
+      raise URI::InvalidURIError, "Hostname not supplied: '#{self}'"
     end
 
     def missing_hierarchical_segment?
@@ -52,11 +73,11 @@ module Addressable
     end
 
     def relative_path?
-      !path.start_with?(URI::SLASH)
+      !path.start_with?(URI::SLASH) && !authority.nil?
     end
 
     def path_with_two_leading_slashes?
-      path.start_with?(URI::DOUBLE_SLASH)
+      path.start_with?(URI::DOUBLE_SLASH) && authority.nil?
     end
 
     def invalid_host_character?
