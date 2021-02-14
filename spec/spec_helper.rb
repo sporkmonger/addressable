85be316905824185bae92ef77644660d5e49cd0e
Patrik Ragnarsson <patrik@starkast.net>
2021-02-15 00:56:33 +0100

    Fail CI when native IDN implementation can't load

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,15 @@ rescue LoadError
   end
 end
 
+class TestHelper
+  def self.native_supported?
+    mri = RUBY_ENGINE == "ruby"
+    windows = RUBY_PLATFORM.include?("mingw")
+
+    mri && !windows
+  end
+end
+
 RSpec.configure do |config|
   config.warnings = true
   config.filter_run_when_matching :focus
