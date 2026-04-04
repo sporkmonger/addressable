93599adec5306abda11aadd2a7c7d2cdfbab5e25
Bob Aman <bob@sporkmonger.com>
2026-04-04 16:37:31 -0500

    Fix a ReDoS vulnerability in URI template matching

--- lib/addressable/version.rb
+++ lib/addressable/version.rb
@@ -23,7 +23,7 @@ if !defined?(Addressable::VERSION)
     module VERSION
       MAJOR = 2
       MINOR = 8
-      TINY  = 9
+      TINY  = 10
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
